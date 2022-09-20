928ccf3684a2aa1284628343b41c8809c65e9ecf
ranselorm <ranselorm@outlook.com>
2022-09-20 07:30:13 +0000

    change environment port to production

--- index.js
+++ index.js
@@ -144,6 +144,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000:");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
 });
